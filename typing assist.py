5791567541358272ac67c737d40377ebb13c982c
ScootiusMaximus <barneyandbagel@gmail.com>
2025-06-03 11:12:59 +0100

    Modified

--- typing assist.py
+++ typing assist.py
@@ -6,6 +6,6 @@ with open("tochange.txt","r",encoding="utf-8") as file:
 for line in lines:
     line = line.replace("\n","")
     bits = line.split(",")
-    kb.add_abbreviation(bits[0],bits[1])
+    kb.add_abbreviation(bits[0],f"{bits[1]} ")
     
 kb.wait()
